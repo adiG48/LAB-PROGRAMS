9d3b49d89958eb6a92fbed2bbe7fd83d0aea268b
Adithyaa.S <72435206+adiG48@users.noreply.github.com>
2020-12-19 15:43:32 +0530

    Update square root by newtons method.py

--- square root by newtons method.py
+++ square root by newtons method.py
@@ -1,13 +1,13 @@
-#HINT
-#formula:
-#x=(x+n/x)/2
-#n=4
-#x0=4
-#x1=(x0+4/x0)/2=2.5
-#x2=(x1+4/x1)/2=2.05
-#x3=(x2+4/x2)/2=2.006
-#x4=(x3+4/x3)/2=2.00
-#square root of 4 = 2
+"""#HINT
+formula:
+x=(x+n/x)/2
+n=4
+x0=4
+x1=(x0+4/x0)/2=2.5
+x2=(x1+4/x1)/2=2.05
+x3=(x2+4/x2)/2=2.006
+x4=(x3+4/x3)/2=2.00
+square root of 4 = 2"""
 def get_sqrt(number,precision):
     sqroot=number
     while abs(number-sqroot*sqroot) > precision:
